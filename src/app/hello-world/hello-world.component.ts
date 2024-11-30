63252ea308e03fe1d61f8c2c9b026169ae64ba91
Hamza-Mughales <hamzawemughales@gmail.com>
2024-11-30 18:09:24 +0300

    One way data binding

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './hello-world.component.css'
 })
 export class HelloWorldComponent {
-
+  isDisabled = false;
+  
+  Company = {
+    name: 'Angular',
+    address: '1600 Amphitheatre Parkway, Mountain View, CA 94043',
+    url: 'https://angular.io'
+  };
+  
+  constructor() {};
 }
